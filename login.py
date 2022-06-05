73e212ba9d624fe58cff6a63026291c142a18292
Sebastian Zumbado H <sebastian.zumbado@ibm.com>
2022-06-04 22:14:13 -0600

    :pencil: Feat: Add CLI parameters to the script. That way we can specify the URL, username and password to use

--- login.py
+++ login.py
@@ -1,8 +1,17 @@
 from modules import web_scrape
+import argparse
 
-username, password = web_scrape.setLoginCredentials("username@domain.com", "password")
+parser = argparse.ArgumentParser(
+    description="Script that will test login to a web page!"
+)
+parser.add_argument("-w", "--webp", default = "https://app.sysdigcloud.com/#/login", help="URL of the login page")
+parser.add_argument("-p", "--password", default = "xyz.edu", help="Password to test")
+parser.add_argument("-u", "--username", default = "username", help="Username to test, email format")
+args = parser.parse_args()
 
-web_scrape.initializeChromeDriver("https://app.sysdigcloud.com/#/login")
+username, password = web_scrape.setLoginCredentials(args.username, args.password)
+
+web_scrape.initializeChromeDriver(args.webp)
 
 web_scrape.testLogin(username, password)
 
